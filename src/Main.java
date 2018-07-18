4d352781f4a0c09c11783eb65d73033911ad630a
anathi.voyi <kwanvoyi@gmail.com>
2018-07-18 11:18:03 +0200

    Add src commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String userName; String afteroon = "Good afternoo"; String morning = "Good morning";String evening = "Good evening";
+        int userAge; int appAge;
+
+
+
+    }
+}
